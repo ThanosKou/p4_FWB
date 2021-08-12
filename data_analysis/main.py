f1445bcb0c2f7356bf1d297a2fad74ce4c9e075e
thanos <koutsaftis.t@gmail.com>
2021-08-12 18:02:32 +0300

    small changes

--- data_analysis/main.py
+++ data_analysis/main.py
@@ -8,15 +8,15 @@ import os
 def compile_data(data_path):
     data_path = '/home/thanos/tutorials/exercises/p4_FWB/out_data'
     files = os.listdir(data_path)
-    gpp3_files = [file for file in files if file.startswith('3gpp_')]
-    fwb_files = [file for file in files if file.startswith('pkt')]
+    gpp3_files = [file for file in files if file.startswith('3gpp_') and file.endswith('_500ms.txt')]
+    fwb_files = [file for file in files if file.startswith('pkt') and file.endswith('_500ms.txt')]
     combined_data_file = 'combined_data.txt'
     wf = open(combined_data_file, "w")
-    wf.write('FlowIdx,ArrivalTime,MulticastIdx,GWDelay,UEDelay,Protocol\n')
+    wf.write('FlowIdx,ArrivalTime,GeneratingTime,FlowDelay,MulticastIdx,GWDelay,UEDelay,Protocol\n')
     protocol_name = '3GPP'
     for gpp_file in gpp3_files:
         rf = open(os.path.join(data_path, gpp_file))
-        lines = rf.readlines()[1:2001]
+        lines = rf.readlines()[1:3001]
         t0 = float(lines[0].split(',')[1])
         gw_delay = float(gpp_file.split('.')[0].split('_')[-2:][0].split('ms')[0])
         ue_delay = float(gpp_file.split('.')[0].split('_')[-2:][1].split('ms')[0])
@@ -24,16 +24,18 @@ def compile_data(data_path):
         for line in lines:
             readings = line.split()[0].split(',')
             flow_idx = float(readings[0])
+	    flow_generating_time = float(readings[1])
             flow_time = float(readings[2])
+	    flow_delay = flow_time - flow_generating_time
             flow_dst_id = float(readings[3])
-            write_str = '{},{},{},{},{},{}\n'.format(flow_idx, flow_time - t0, flow_dst_id, gw_delay, ue_delay,
+            write_str = '{},{},{},{},{},{},{},{}\n'.format(flow_idx, flow_time - t0, flow_generating_time, flow_delay, flow_dst_id, gw_delay, ue_delay,
                                                      protocol_name)
             wf.write(write_str)
         rf.close()
     protocol_name = 'FWB'
     for fwb_file in fwb_files:
         rf = open(os.path.join(data_path, fwb_file))
-        lines = rf.readlines()[1:2000]
+        lines = rf.readlines()[1:3000]
         t0 = float(lines[0].split(',')[1])
         gw_delay = float(fwb_file.split('.')[0].split('_')[-2:][0].split('ms')[0])
         ue_delay = float(fwb_file.split('.')[0].split('_')[-2:][1].split('ms')[0])
@@ -41,9 +43,11 @@ def compile_data(data_path):
         for line in lines:
             readings = line.split()[0].split(',')
             flow_idx = float(readings[0])
+	    flow_generating_time = float(readings[1])
             flow_time = float(readings[2])
+	    flow_delay = flow_time - flow_generating_time
             flow_dst_id = float(readings[3])
-            write_str = '{},{},{},{},{},{}\n'.format(flow_idx, flow_time - t0, flow_dst_id, gw_delay, ue_delay,
+            write_str = '{},{},{},{},{},{},{},{}\n'.format(flow_idx, flow_time - t0, flow_generating_time, flow_delay, flow_dst_id, gw_delay, ue_delay,
                                                      protocol_name)
             wf.write(write_str)
         rf.close()
@@ -51,11 +55,11 @@ def compile_data(data_path):
 
 
 if __name__ == '__main__':
-    data_path = '/home/thanos/tutorials/exercises/p4_FWB/out_data'
-    compile_data(data_path)
+    #data_path = '/home/thanos/tutorials/exercises/p4_FWB/out_data'
+    #compile_data(data_path)
     df = pd.read_csv('combined_data.txt')
     df['GW Delay (ms), UE Delay (ms)'] = df['GWDelay'].astype('int').astype('str')+ ', ' + df['UEDelay'].astype('int').astype('str')
-    df_small = df[0:4000]
+    df_small = df[0:6000]
     df_ue = df.loc[df.loc[:, 'UEDelay'] == 500, :]
     sns.lineplot(x='Packet Index', y='Arrival Time', style='Protocol', hue='GW Delay (ms), UE Delay (ms)', data=df_ue.sort_values(['GWDelay']), markers=False)
     fig1 = plt.gcf()
