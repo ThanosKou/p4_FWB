1045c470eac400c83acd7ba0d274225229f30721
ThanosKou <koutsaftis.t@gmail.com>
2021-12-11 20:03:17 -0500

    Update BS2.py

--- BS2.py
+++ BS2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import argparse
 import sys
 import socket
@@ -79,17 +79,17 @@ def handle_pkt(pkt):
             notification_pkt = e / fwb(dst_id=pkt[fwb].dst_id, pkt_id=0,
                 pid=TYPE_IPV4)/IP(dst='10.0.1.1')/ TCP(dport=2222, sport=50004) / 'Notifying h1 (GW), BS2 is primary now Changed at packet idx {}'.format(ue_asks)
             sendp(notification_pkt,iface=iface,verbose=False)
-	    notification_pkt = e / fwb(dst_id=pkt[fwb].dst_id,pkt_id=0,
+        notification_pkt = e / fwb(dst_id=pkt[fwb].dst_id,pkt_id=0,
                 pid=TYPE_IPV4)/IP(dst='10.0.2.2')/ TCP(dport=2223, sport=50004) / 'Notifying UE, BS1 is primary now Changed at packet idx {}'.format(ue_asks)
             sendp(notification_pkt,iface=iface,verbose=False)
             # notification_pkt.show()
         elif pkt[IP].src =='10.0.1.1' and pkt[TCP].dport==1111: #received data packet
             current_state = BS_2_dst_id_map(pkt[fwb].dst_id)
             if current_state == 'secondary':
-            	pkt_idx = pkt[fwb].pkt_id
-            	my_buffer[w_idx] = [pkt_idx,str(bytes(pkt[TCP].payload))]
-            	w_idx = (w_idx+1)%BUFFER_LEN
-            	if isTransition:
+                pkt_idx = pkt[fwb].pkt_id
+                my_buffer[w_idx] = [pkt_idx,str(bytes(pkt[TCP].payload))]
+                w_idx = (w_idx+1)%BUFFER_LEN
+                if isTransition:
                     fwd_pkt = buffer_pkt_fwd(pkt)
                     # fwd_pkt.show()
                     current_state = 'primary'
@@ -114,8 +114,8 @@ def handle_pkt(pkt):
 def main():
     global iface
     ifaces = filter(lambda i: 'eth0' in i, os.listdir('/sys/class/net/'))
-    iface = ifaces[0]
-    print "base station: using %s" % iface
+    iface = next(ifaces)
+    print("base station: using " + iface)
     global e
     global last_received
     global pkt_barebone
