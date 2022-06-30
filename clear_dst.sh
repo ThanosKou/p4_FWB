c833321167dc0dd6f67f421f23598e7ec488b173
ThanosKou <apk337@nyu.edu>
2022-06-30 16:16:44 -0400

    delete old files

--- clear_dst.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo 1 0 0 > dst_holder.txt
-sleep 1
--- init_script.sh
+++
@@ -1,8 +0,0 @@
-h1 ./clear_dst.sh
-h1 ./listen_and_update.py &
-h3 ./BS1.py &
-h4 ./BS2.py &
-h1 ./3gpp_listen_and_update.py &
-h2 ./v2_UE-listener.py &
-h1 ./reader_sender.py &
-h2.monitor()
