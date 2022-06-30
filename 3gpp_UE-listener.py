8a5c2236693752bf6043f63c47a99dacec3d2ab8
ThanosKou <apk337@nyu.edu>
2022-06-30 16:00:52 -0400

    corrections

--- 3gpp_UE-listener.py
+++ 3gpp_UE-listener.py
@@ -58,7 +58,6 @@ def handle_pkt(pkt):
     global recording_file
     global received_packets
     global t0
-    global transition
     global sleeping_time
     if fwb in pkt:
         if pkt[IP].dst == '10.0.2.2' and pkt[TCP].dport == 1111: # 1111 data layer
@@ -66,15 +65,13 @@ def handle_pkt(pkt):
             if pkt[fwb].dst_id in a_m_idx[prev_dst] and pkt[fwb].pkt_id not in received_packets:
                 received_packets.append(pkt[fwb].pkt_id)
                 last_received = pkt[fwb].pkt_id
-                if last_received == event_idx:
-                    transition = True
-                print('{},{},{},{}\n'.format(last_received,generated_time,time.time()-t0,pkt[fwb].dst_id))
+                print('{},{},{},{}\n'.format(last_received,generated_time,time.time()-t0-generated_time,pkt[fwb].dst_id))
                 recording_file.write('{},{},{},{}\n'.format(last_received,generated_time,time.time()-t0,pkt[fwb].dst_id))
                 if last_received >= 10000:
                     print('Done')
                     exit()
             if last_received == event_idx:
-                print('PKT IDX:{}, NXT_DST:{}'.format(last_received,str(time.time()-t0)))
+                print('PKT IDX:{}, NXT_DST:{}'.format(last_received,str(time.time()-t0-generated_time)))
                 next_dst = int(np.random.choice(np.array(transitions[prev_dst])))
                 outage_delay = 0 
                 if next_dst == 4:
@@ -99,8 +96,7 @@ def main():
     global received_packets
     transitions = [[2,3],[2,3],[0,4],[1,4],[2,3]]
     #transitions = [[2,3],[2,3],[0],[1],[2,3]]
-    event_idx = -1
-    #event_idx = random.randint(50,60)
+    event_idx = random.randint(50,60)
     a_m_idx = [[0,2],[1,3],[2,0],[3,1],[2,3]] # acceptable multicast ...
     #destinations for a prev_dst, for example adding a secondary bs or removing the secondary bs should still be valid even if prev_dst is different
     global recording_file
@@ -114,7 +110,7 @@ def main():
         topo = json.load(f)
     GW_delay = topo['links'][0][2]
     UE_delay = topo['links'][1][2]
-    record_string = Path.cwd()/"out_data"/"test"/"3gpp_pkt_arrivals_{}ms_{}ms.txt".format(GW_delay,UE_delay)
+    record_string = Path.cwd()/"out_data"/"3gpp_pkt_arrivals_{}ms_{}ms.txt".format(GW_delay,UE_delay)
     recording_file = open(record_string, "w")
     recording_file.write('PacketSeqNo,GeneratedTime(sec),ArrivalTime(sec),MulticastIdx\n')
 
@@ -130,9 +126,8 @@ def main():
     global pkt_control_bbone_t0
     global prev_dst
     global sleeping_time 
-    transition = False
     prev_dst = 1 #always start with case 1
-    sleeping_time = 1
+    sleeping_time = 2
     e =  Ether(src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff', type=TYPE_FWB)
     pkt_control_bbone =  TCP(dport=2222, sport=50002) / ''
     pkt_control_bbone_t0 =  TCP(dport=2223, sport=50002) / ''
--- 3gpp_init_script.sh
+++ 3gpp_init_script.sh
@@ -1,1 +1,5 @@
-xterm h1 h1 h2 
+h1 ./3gpp_clear_dst.sh
+h1 ./3gpp_listen_and_update.py &
+h2 ./3gpp_UE-listener.py &
+h1 ./3gpp_reader_sender.py &
+h2.monitor()
